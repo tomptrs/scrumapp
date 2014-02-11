3501857e8374fb30abfe30f084365a530faf9b73
tomptrs <tom_ptrs@yahoo.com>
2014-02-11 10:06:37 +0100

    json test

--- a.php
+++ a.php
@@ -7,7 +7,17 @@ $m = new Member();
 //$json = array();
 $str = $m->GetData();
 
-print $str;
+echo json_encode($str);
+//print $str;
+
+/*
+ $value = array('return' => 1, 'msg1' => 'Message sent OK, we will be in touch ASAP');
+} else {
+    $value = array('return' => 0, 'msg1' => 'Message Failed, please try later');
+}
+$output = $json->encode($value);
+echo $output;
+*/
 
 
 //TRY TO SEND JSON DATA!
--- members.php
+++ members.php
@@ -18,18 +18,21 @@ class Member
 		$this->db->query($sql);		
 		
 		$json = array();
-		
+		$teller = 0;
 		while ($line = $this->db->fetchArray() )
 		{
+			//$json[$teller] = "
+			$teller++;
 		//print $line[0];
-		print ' <a href="#" class="list-group-item">'.$line[1] . ' ' . $line[2].'</a>';
+//		print ' <a href="#" class="list-group-item">'.$line[1] . ' ' . $line[2].'</a>';
 			//$json[] = $line;
 			
 		}
 		
 			//print $json[0];
 		//	return json_encode($json);	
-
+	$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+	return $arr;
 	}
 	
 	public function InsertUserInDB($name)
