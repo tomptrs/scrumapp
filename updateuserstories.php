86af8691da3a6d07651a61670f23569510fd9383
tomptrs <tom_ptrs@yahoo.com>
2014-02-14 12:03:05 +0100

    ok

--- updateuserstories.php
+++ updateuserstories.php
@@ -10,13 +10,13 @@ print $_GET["id"];
 $id 	= 0;
 $status = 0;
 
-if( isset($_GET["id"]) )// && isset($_GET["status"]))
-{
+//if( isset($_GET["id"]) )// && isset($_GET["status"]))
+//{
 	$id = $_GET["id"];
 	print $id;
 //	$status = $_GET["status"];
 //	print $status;
-}
+//}
 
 $m = new UserStory();			
 $m->UpdateUserStory($id,$status);
