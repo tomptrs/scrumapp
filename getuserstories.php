cf2ef3c2163c0b85015b26217f00df1a90f1e806
tomptrs <tom_ptrs@yahoo.com>
2014-02-14 09:22:12 +0100

    test

--- getuserstories.php
+++ getuserstories.php
@@ -1,16 +1,19 @@
 <?php
 
+print "fuck";
+/*
 require_once "userstories.php";
+
 $m = new UserStory();			
 
 //header('Content-Type: application/json');
 header("Content-type: text/javascript");
-//$json = array();
+
 print "test";
 $str = $m->GetStories();
 print "test";
 echo json_encode($str);
-
+*/
 
 
 //TRY TO SEND JSON DATA!
