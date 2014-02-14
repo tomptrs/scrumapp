09d5f2ad2deb4a245c9835c4bc196defa36aab82
tomptrs <tom_ptrs@yahoo.com>
2014-02-14 11:56:04 +0100

    test

--- updateuserstories.php
+++ updateuserstories.php
@@ -3,16 +3,18 @@
 require_once "userstories.php";
 
 $id ,$status;
-
+print "test";
 if( isset($_GET["id"]) && isset($_GET["status"]))
 {
 	$id = $_GET["id"];
+	print $id;
 	$status = $_GET["status"];
-}
+	print $status;
+
 
 $m = new UserStory();			
 $m->UpdateUserStory($id,$status);
-
+}
 ?>   
    
 
