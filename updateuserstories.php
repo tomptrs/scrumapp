b5cf15e821256933412ef9961999401f00ea6e5f
tomptrs <tom_ptrs@yahoo.com>
2014-02-14 15:17:12 +0100

    ok

--- updateuserstories.php
+++ updateuserstories.php
@@ -7,19 +7,19 @@ require_once "userstories.php";
 $v= 666;
 $status = 444;
 
-if( isset($_GET["id"]) ) && isset($_GET["status"]))
+if( isset($_GET["id"])  && isset($_GET["status"]))
 {
 	$v = $_GET["id"];
-	//print $v;
+	print $v;
 	$status = $_GET["status"];
-	//	print $status;	
+		print $status;	
 	$m = new UserStory();			
 	$m->UpdateUserStory($v,$status);
 
-}
+
 
 }
-*/
+
 ?>   
    
 
