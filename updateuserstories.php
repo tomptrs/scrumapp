bfc1d0bdc755db4ae8d0eb2e78808a39e603a815
tomptrs <tom_ptrs@yahoo.com>
2014-02-14 15:02:56 +0100

    jfkml

--- updateuserstories.php
+++ updateuserstories.php
@@ -4,22 +4,20 @@
 require_once "userstories.php";
 
 
-print "test";
-print $_GET["id"];
+$v= 666;
+$status = 444;
 
-
-
-
-//if( isset($_GET["id"]) )// && isset($_GET["status"]))
-//{
-	//$v = $_GET["id"];
+if( isset($_GET["id"]) ) && isset($_GET["status"]))
+{
+	$v = $_GET["id"];
 	//print $v;
-//	$status = $_GET["status"];
-//	print $status;
-//}
+	$status = $_GET["status"];
+	//	print $status;	
+	$m = new UserStory();			
+	$m->UpdateUserStory($v,$status);
 
-$m = new UserStory();			
-$m->UpdateUserStory($id,$status);
+}
+
 }
 */
 ?>   
