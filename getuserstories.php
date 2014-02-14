883e96f2bb557642d75324f3a53fb9d7756a7cc7
tomptrs <tom_ptrs@yahoo.com>
2014-02-14 09:22:48 +0100

    test 2

--- getuserstories.php
+++ getuserstories.php
@@ -1,11 +1,13 @@
 <?php
 
 print "fuck";
-/*
+
 require_once "userstories.php";
 
 $m = new UserStory();			
 
+print "dubbel f";
+/*
 //header('Content-Type: application/json');
 header("Content-type: text/javascript");
 
