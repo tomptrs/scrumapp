a5973df5257510730e830fad5010559e0eece251
tomptrs <tom_ptrs@yahoo.com>
2014-02-16 15:15:28 +0100

    aws email library

--- email.php
+++ email.php
@@ -1,30 +1,52 @@
 <?php
 	
-	mysql_connect("localhost", "mailapp", "mailpass") 
-	    or die(mysql_error());
-	mysql_select_db("test") 
-	    or die(mysql_error());
+	include "aws-sdk-for-php-master\sdk.class.php";
 	
-	$result = mysql_query("select * from customers")
-	    or die(mysql_error());  
 	
-	while ($row = mysql_fetch_array($result)) {
+	function amazonSesEmail($to, $subject, $message)
+{
+    $amazonSes = new AmazonSES(array(
+        'key' => "AKIAILTXZOTR7G62RPGQ",
+        'secret' => "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"
+    ));
+ 
+ $subject = "test";
+ $message = "test message";
+    $response = $amazonSes->send_email("tom_ptrs@yahoo.com",
+        array('tom.peeters@ap.be' => array($to)),
+        array(
+            'Subject.Data' => $subject,
+            'Body.Text.Data' => $message,
+        )
+    );
+    if (!$response->isOK())
+    {
+        // handle error
+    }
+}
+//	mysql_connect("localhost", "mailapp", "mailpass") 
+//	    or die(mysql_error());
+//	mysql_select_db("test") 
+//	    or die(mysql_error());
+	
+	//$result = mysql_query("select * from customers")
+	 //   or die(mysql_error());  
+	
+	//while ($row = mysql_fetch_array($result)) {
              
-	    $headers = "From: bob@example.com\n";
+	    $headers = "From: tom_ptrs@yahoo.com\n";
 	
-	    $subject = "Thank you " . $row['first_name'] . "!";
+	    $subject = "Thank you TEST!";
 	
-	    $message = "Dear " . $row['first_name'] . ",\n\n"; 
+	    $message = "Dear TEST \n\n"; 
 	    $message .= "Thank you for being a loyal customer. " . 
                 "Please contact us at 555-4321 for a special discount offer.";
 	
-	    mail($row['email_address'], $subject, $message, $headers);
+	    mail("tom.peeters@ap.be", $subject, $message, $headers);
 
-	    echo "Sent email to:  " .
-                $row['first_name'] . " " . $row['last_name'] .
-                "(" . $row['email_address'] . ")" . "<br>";
+	    echo "Sent email to";
 	    
-	    sleep(5);
-	}
+	   // sleep(5);
+	//}
 	
 ?>
